ff7c8f25f56024c78370bb00214ce951720f75bc
junior <juniormathiias@gmail.com>
2023-06-09 13:27:22 -0300

    creating some style and the component and page

---
+++ src/pages/address/index.jsx
@@ -0,0 +1,12 @@
+
+import styles from './address.module.css'
+import { Header } from '../../components/header'
+export function Address() {
+
+  return (
+    <>
+    <Header/>
+   
+    </>
+  )
+}
